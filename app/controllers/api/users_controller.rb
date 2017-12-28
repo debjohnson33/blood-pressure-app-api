dcbb26e795796f8147b5ed4f613f61c8015c05f7
debjohnson33 <debjohnson_11@yahoo.com>
2017-12-28 15:14:05 -0600

    Added user request is valid test spec

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,4 +4,16 @@ class Api::UsersController < ApplicationController
 		render json: users, status: 200
 	end
 
+	def create
+		user = User.new(user_params)
+		if user.save
+			render json: user, status:201
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :gender, :birthdate)
+	end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -21,7 +21,41 @@ RSpec.describe 'Measurements API', type: :request do
 			expect(json.size).to eq(5)
 		end
 	end
-	# POST /api/user (new measurement)
+	# POST /api/users (new measurement)
+
+	describe 'POST /api/users' do
+
+		context 'when the request is valid' do
+
+			let(:valid_attributes) {
+				{
+					user: {
+						first_name: Faker::Name.first_name, 
+	    				last_name: Faker::Name.last_name,
+	    				gender: Faker::Demographic.sex,
+	    				birthdate: "1989-03-02T00:00:00.000Z"
+					}
+				}
+			}
+			before { post '/api/users', params: valid_attributes }
+
+			it 'returns a status code of 201' do
+				expect(response).to have_http_status(201)
+			end
+			it 'creates a user and returns it in JSON' do
+				json = JSON.parse(response.body, symbolize_names: true)
+
+				expect(json).not_to be_empty
+				expect(json[:id]).not_to eq(nil)
+				expect(json[:first_name]).to eq(valid_attributes[:user][:first_name])
+				expect(json[:last_name]).to eq(valid_attributes[:user][:last_name])
+				expect(json[:gender]).to eq(valid_attributes[:user][:gender])
+				expect(json[:birthdate]).to eq(valid_attributes[:user][:birthdate])
+			end
+		end
+
+		context 'when the request is invalid'
+	end
 	# GET /api/user/:id
 	# PUT /api/user/:id
 	# DELETE /api/user/:id
