1b1e660074891e93ced0a4fd7643d1ebee116667
debjohnson33 <debjohnson_11@yahoo.com>
2017-12-28 13:54:43 -0600

    Started measurement api request spec/added routes

---
+++ spec/requests/measurement_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Measurements API', type: :request do
+	# GET /api/user/measurements
+
+	describe 'GET /api/user/measurements' do
+		
+		before { get '/api/user/measurements' }
+
+		it 'returns a status code of 200' do
+			expect(response).to have_http_status(200)
+		end
+
+		it 'returns a collection of measurements for that user in JSON'
+	end
+	# POST /api/user/measurements (new measurement)
+	# GET /api/user/measurements/:id
+	# PUT /api/user/measurements/:id
+	# DELEATE /api/user/measurements/:id
+
+
+end
