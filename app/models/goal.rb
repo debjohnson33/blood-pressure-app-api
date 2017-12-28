653262b8dd80e9e4c070536bc9a80a1a27248506
debjohnson33 <debjohnson_11@yahoo.com>
2017-12-27 19:07:02 -0600

    Added model relationship

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,4 @@
 class Goal < ApplicationRecord
-	validates :systolic_bp, :diastolic_bp, :frequency, :user_id, presence: true
 	belongs_to :user
+	validates :systolic_bp, :diastolic_bp, :frequency, :user_id, presence: true
 end
