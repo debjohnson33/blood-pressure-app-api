d790fedfc23c18bf2d26c83514bd03079cf9fa84
debjohnson33 <debjohnson_11@yahoo.com>
2017-12-29 13:16:26 -0600

    Added/installed/configured devise & added home root route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :measurements, dependent: :destroy
 	has_one :goal
 	validates :first_name, :last_name, :gender, :birthdate, presence: true
